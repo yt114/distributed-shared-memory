fdd003a620fe2943c1ca12b4643a483ed764a152
yt114 <yxt5200@psu.edu>
2020-12-11 21:38:05 -0500

    paxos

--- paxos_client.cpp
+++ paxos_client.cpp
@@ -64,6 +64,46 @@ int paxos_put(struct Client* c, string key, string& value){
 
 }
 
+
+int paxos_get(struct Client* c, string key, string& value){
+    stringstream ss;
+    ss<<"read write on key "<<key<<endl;
+
+    int server_id = c->id % c->number_of_servers;
+
+    string server_addr(c->servers[server_id].ip);
+    server_addr += ":" + to_string(c->servers[server_id].port);
+
+    cout<<"send put request to server_id "<<server_id << " addr "<<server_addr<<endl;
+    shared_ptr<Channel> toserverChannel = grpc::CreateChannel(server_addr,
+                                                              grpc::InsecureChannelCredentials());
+
+    unique_ptr<PaxosProposer::Stub> stub_ = PaxosProposer::NewStub(toserverChannel);
+
+    ClientRequestPacket request; ServerResponsePacket reply; ClientContext context;
+    request.set_is_write(false);
+    request.set_key(key);
+    //request.set_value(value);
+    request.set_client_id(c->id);
+    request.set_client_command_id(c->command_id);
+
+    c->command_id ++;
+
+    Status status = stub_->client_request(&context, request, &reply);
+    if (status.ok()){
+        ss<<"get return success flag value: "<<reply.is_success()<<endl;
+        value = reply.value();
+    }
+
+    ss<<" read finishes"<<endl;
+
+    ss<<"---------------------------------------------------------------------"<<endl;
+    cout<<ss.str()<<endl;
+
+    return reply.is_success();
+
+}
+
 int put(struct Client* c, const char* key, uint32_t key_size, const char* value, uint32_t value_size){
     std::ofstream logfile;
     stringstream temp;
@@ -96,3 +136,31 @@ int put(struct Client* c, const char* key, uint32_t key_size, const char* value,
     return 0;
 }
 
+int get(const struct Client* c, const char* key, uint32_t key_size, char** value, uint32_t *value_size){
+    string updateKey;
+    for (int i=0; i<key_size; i++){
+        updateKey += key[i];
+    }
+
+    std::ofstream logfile;
+    char logfilename[30];
+    duration<double> currentTime_span;
+    sprintf(logfilename, "log_%d.txt", c->id);
+    logfile.open(logfilename, std::ofstream::out | std::ofstream::app);
+
+    currentTime_span = duration_cast<duration<double>>( high_resolution_clock::now() - beginTime);
+    logfile<< currentTime_span.count() <<" {:process "<< c->id << ", :type :invoke, "
+           << ":f :read," << " :value nil}" << std::endl;
+    string ret_value;
+
+    paxos_get(c, updateKey, ret_value);
+    currentTime_span = duration_cast<duration<double>>( high_resolution_clock::now() - beginTime);
+    logfile<< currentTime_span.count() <<" {:process "<< c->id << ", :type :ok, "
+           << ":f :read,"  << " :value " <<ret_value<<"}"<< std::endl;
+    *value_size = ret_value.size() + 1;
+    char * c_val = new char[*value_size];
+    strcpy(c_val, ret_value.c_str());
+    *value = c_val;
+
+    return 0;
+}
